d5b066eed1b429cdec9049a1a8616e2cdc4e4f22
Ahmad Fadhel <ahmad.f.ameen@gmail.com>
2021-05-03 10:36:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,14 +9,14 @@ class Circle {
     return this.radius * 2
   }
   get circumference(){
-    return this.pi * this.diameter
+    return this.pi * diameter
   }
   get area(){
-    return this.pi * this.radius^2
+    return this.pi * this.radius**2
   }
 }
 
-let circle = new Circle(6)
+let circle = new Circle(18)
 circle.diameter;
 circle.circumference;
 circle.area
