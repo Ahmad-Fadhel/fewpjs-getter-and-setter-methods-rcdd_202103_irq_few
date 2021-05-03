93b0513e2a7a1f06fb9025aa8d183c8862fe2bdb
Ahmad Fadhel <ahmad.f.ameen@gmail.com>
2021-05-03 10:29:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Add your Circle class here
+class Circle {
+  constructor(radius){
+    this.radius = radius;
+    this.pi = Math.pi;
+  }
+  
+  get diameter(){
+    return radius * 2
+  }
+  get diameter(){
+    return radius * 2
+  }
+  get diameter(){
+    return radius * 2
+  }
+}
+
+
